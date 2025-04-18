adf3e0ed4e9e05274618439c34d42b22e445ba3d
Sunidhi Soner <sunidhisinghsoner@gmail.com>
2025-04-18 16:38:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// let div = document.querySelector("div");
-// let ul = document.querySelector("ul");
-// let lis = document.querySelectorAll("li");
-
-// div.addEventListener("click", function() {
-//     console.log("div was clicked");
-// });
-
-// ul.addEventListener("click", function(event) {
-//     event.stopPropogation();
-//     console.log("ul was clicked");
-// });
-
-// for(li of lis){
-//     li.addEventListener("click", function(event) {
-//         event.stopPropogation();
-//         console.log("li was clicked");
-//     });
-// }
-
 let inp = document.querySelector("input");
 let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
